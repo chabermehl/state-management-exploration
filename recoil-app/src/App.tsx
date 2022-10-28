03a6b66aabae2dc68f59d9b8d9f22afa2f72780f
Charles <chabermehl@indicalab.com>
2022-10-28 07:40:16 -0600

    Add prettierrc, set up basic recoil app

--- recoil-app/src/App.tsx
+++ recoil-app/src/App.tsx
@@ -1,10 +1,57 @@
 import React from "react";
-import {RecoilRoot} from 'recoil'
+import {atom, RecoilRoot, selector, useRecoilState, useRecoilValue} from 'recoil'
+
+const textState = atom({
+  key: 'textState',
+  default: '',
+});
+
+const charCountState = selector({
+  key: 'charCountState', 
+  get: ({get}) => {
+    const text = get(textState);
+
+    return text.length;
+  },
+});
+
+const TextInput: React.FC = () => {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
+
+const CharacterCount: React.FC = () => {
+  const count = useRecoilValue(charCountState);
+
+  return <React.Fragment>Character Count: {count}</React.Fragment>;
+}
+
+const CharacterCounter: React.FC = () => {
+  return (
+    <div>
+      <TextInput />
+      <CharacterCount />
+    </div>
+  );
+}
+
+
 
 const App: React.FC = () => {
   return (
    <RecoilRoot>
-      <h1>RECOIL</h1>
+      <CharacterCounter />
     </RecoilRoot>
   );
 };
